203a1a3f45c6bd81ef7a562ca2a5a50b8ad9da3a
venkat <venkatsatya>
2022-06-14 15:44:23 +0530

    Venkat

--- GitDemo/src/main/java/com/example/demo/GitDemoApplication.java
+++ GitDemo/src/main/java/com/example/demo/GitDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hai");
+		System.out.println("Welcome");
 		SpringApplication.run(GitDemoApplication.class, args);
 	}
 
